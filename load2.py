d120fb5ddca77e88749b60bc82c52910839ca044
TimothyHonablew <61149729+TimothyHonablew@users.noreply.github.com>
2023-12-20 15:11:35 -0500

    Add files via upload

--- load2.py
+++ load2.py
@@ -8,5 +8,6 @@ r = 8 * 10**10;     #Name: Radius
 P = 1;              #Name: Pressure
 T = 1;              #Name: Temperature
 
+#This functions changes the initial guesses by half of the calculated percent differences to try to reach convergence
 def intialSurface(diffL, diffR, diffP, diffT):
     return L * (1 - diffL/2), r * (1-diffR/2), P * (1-diffP/2), T * (1-diffT/2);
